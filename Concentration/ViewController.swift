90a95d8c6283b95fa5b896fd70207de067af1b27
Ahmad Raza <raza2157@gmail.com>
2019-03-27 00:37:08 -0400

    Initialized repo

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🏆", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
